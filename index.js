3f93fbc4c2e9d4cfea926c2ee915322c30314e5e
PratikFandade <pratik.fandade18@vit.edu>
2022-04-18 15:17:47 +0530

    Ready to Deploy!

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-if(process.env.NODE_ENV == 'production') {
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
-
 // Connecting MongoDB Datbase
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
@@ -30,6 +22,14 @@ mongoose.connect(uri, {
 // Hosting Server & Connecting Routes
 const wordsRouter = require('./routes/words')
 app.use('/words', wordsRouter)
+
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
